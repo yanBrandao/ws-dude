c8165b457c95e5176887ab7918451e53548e4d56
Yan Brandão Tapajós <ybrandao.d@gmail.com>
2019-10-11 11:44:48 -0400

    Added spring-boot project with Undertow server

---
+++ server/src/main/java/com/dudes/wsdude/WsDudeApplication.java
@@ -0,0 +1,13 @@
+package com.dudes.wsdude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsDudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsDudeApplication.class, args);
+	}
+
+}
